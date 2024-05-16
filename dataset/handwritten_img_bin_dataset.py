5d84935bdc0a7bfec1708a3a04e2cf2005adbb25
dwd <monday0219@sina.com>
2024-05-16 11:45:38 +0800

    添加崩溃信息

--- dataset/handwritten_img_bin_dataset.py
+++ dataset/handwritten_img_bin_dataset.py
@@ -195,6 +195,7 @@ class HandWrittenBinDataSet(IterableDataset):
         if self.__index >= len(self.__X):
             print(f"数据错误，输出当前的变量", 
                   f"\nself.__current_bin_index = {self.__current_bin_index}", 
+                  f"\nbin = {self.__y_bin_files[self.__current_bin_index]}",
                   f"\nself.__index = {self.__index}", 
                   f"\nlen(self.__X) = {len(self.__X)}", 
                   f"\nlen(self.__y) = {len(self.__y)}")
@@ -203,6 +204,7 @@ class HandWrittenBinDataSet(IterableDataset):
         if self.__index >= len(self.__y):
             print(f"数据错误，输出当前的变量", 
                   f"\nself.__current_bin_index = {self.__current_bin_index}", 
+                  f"\nbin = {self.__y_bin_files[self.__current_bin_index]}",
                   f"\nself.__index = {self.__index}", 
                   f"\nlen(self.__X) = {len(self.__X)}", 
                   f"\nlen(self.__y) = {len(self.__y)}")
